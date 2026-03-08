5a82bfafe8d28a36d17b0cbb084e370b5360572b
chinnukush <chinnukushal123@gmail.com>
2026-03-08 20:38:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,7 +67,7 @@ VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/HK_How_To_Open/19
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "https://harisample002.blogspot.com/2026/03/file-store.html") # For More Information Check Video On Yt - @Tech_VJ
+WEBSITE_URL = environ.get("WEBSITE_URL", "https://harimovieshm.blogspot.com/2026/03/harimoviez.html") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
 STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
