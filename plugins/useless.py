cafaf05d1043564eff44929f7d6f30ea30ca6ccb
chinnukush <chinnukushal123@gmail.com>
2026-03-10 11:32:14 +0530

    Create useless.py

---
+++ plugins/useless.py
@@ -0,0 +1,39 @@
+import asyncio
+import os
+import random
+import sys
+import time
+from datetime import datetime, timedelta
+from pyrogram import Client, filters, __version__
+from pyrogram.enums import ParseMode, ChatAction
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ReplyKeyboardMarkup, ChatInviteLink, ChatPrivileges
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserNotParticipant
+from bot import Bot
+from config import *
+from helper_func import *
+from database.database import *
+
+#=====================================================================================##
+
+@Bot.on_message(filters.command('stats') & admin)
+async def stats(bot: Bot, message: Message):
+    now = datetime.now()
+    delta = now - bot.uptime
+    time = get_readable_time(delta.seconds)
+    await message.reply(BOT_STATS_TEXT.format(uptime=time))
+
+
+#=====================================================================================##
+
+WAIT_MSG = "<b>Working....</b>"
+
+#=====================================================================================##
+
+
+@Bot.on_message(filters.command('users') & filters.private & admin)
+async def get_users(client: Bot, message: Message):
+    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
+    users = await db.full_userbase()
+    await msg.edit(f"{len(users)} users are using this bot")
+
