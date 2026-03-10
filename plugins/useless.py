5b724042fcc11aef579591d855b62a1441b99fe4
chinnukush <chinnukushal123@gmail.com>
2026-03-10 18:58:59 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -18,34 +18,13 @@ admin = filters.user(ADMINS)
 
 #=====================================================================================##
 
-START_TIME = datetime.now()
-
-@Client.on_message(filters.command("stats") & admin)
-async def stats(client, message: Message):
-    total = await db.total_users_count()
-
-    now = datetime.now()
-    delta = now - START_TIME
-    uptime = get_readable_time(int(delta.total_seconds()))
-
-    await message.reply(
-        f"""📊 **BOT STATISTICS**
-
-👥 Total Users : {total}
-⏰ Uptime : {uptime}
-🤖 Pyrogram : {__version__}""")
-
-
-#=====================================================================================##
-
 WAIT_MSG = "<b>Working....</b>"
 
 #=====================================================================================##
-
-
 @Client.on_message(filters.command("users") & filters.private & admin)
 async def get_users(client, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
     users_cursor = await db.get_all_users()
     users = await users_cursor.to_list(length=None)  # convert cursor to list
     await msg.edit(f"{len(users)} users are using this bot")
+#=====================================================================================##
